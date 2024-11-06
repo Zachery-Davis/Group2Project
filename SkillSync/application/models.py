b164c1fca6fd57e61d29c9854e65c30b1bb94f79
Andrew Hausler <81341702+andrewhausler@users.noreply.github.com>
2024-11-06 16:13:29 -0500

    Django Project Setup

--- SkillSync/application/models.py
+++ SkillSync/application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    firstName = models.CharField(max_length=200, null=True)
+    lastName = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(max_length=500, null=True)
+    
+    REQUIRED_FIELDS = ["firstName", "lastName", "email"]
+
+
