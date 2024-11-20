bcdc62d6862d5662c7c5331e175aa74cd07018e5
Zachery Davis <zacherydavis@hotmail.com>
2024-11-20 16:52:33 -0500

    Login Form

--- SkillSync/application/models.py
+++ SkillSync/application/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     password = models.CharField(max_length=100)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "password"]
+    REQUIRED_FIELDS = []
+    
 
 
--- SkillSync/application/static/js/login.js
+++ SkillSync/application/static/js/login.js
@@ -3,24 +3,27 @@ const regInput = document.querySelector("#regInput");
 const confirmPassword = regInput.querySelector("input");
 const f1Button = document.querySelector(".F1-button");
 const f2Button = document.querySelector(".F2-button");
+const header = document.querySelector("h2");
+const password = document.getElementById("password");
 
 f2Button.addEventListener("click", (event) => {
-  const isLogin = f1Button.innerText === "Login";
+  const isLogin = header.innerText === "Login";
 
   // Toggle between login and register states
   regInput.style.display = isLogin ? "block" : "none";
   confirmPassword.required = isLogin;
+  document.getElementById("password").name = isLogin ? "password1" : "password";
   f1Button.innerText = isLogin ? "Register" : "Login";
   f2Button.innerText = isLogin ? "Login" : "Don't have an account? Register";
-  document.querySelector("h2").innerText = isLogin ? "Register" : "Login";
+  header.innerText = isLogin ? "Register" : "Login";
 });
 
 document.querySelector("form").addEventListener("submit", function (event) {
   // Get the current form action
-  const formAction = document.querySelector("form").getAttribute("action");
+  const isLogin = header.innerText === "Register";
 
   // If the form is set to "register", ensure both passwords match
-  if (formAction === "register") {
+  if (formAction === "Register") {
     const password = document.getElementById("password");
     const confirmPassword = document.getElementById("password2");
 
